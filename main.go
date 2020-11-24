67d0853e002677112672ed301fca8b2f4bc0b321
birchgrove <birchgrove0612@gmail.com>
2020-11-24 18:06:37 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
-func myWeb(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "这是一个开始")
+func main() {
+	http.HandleFunc("/", hello)
+	server := &http.Server{
+		Addr: ":8888",
+	}
+  fmt.Println("server startup...")
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Printf("server startup failed, err:%v\n", err)
+	}
 }
 
-func main() {
-    http.HandleFunc("/", myWeb)
-
-    fmt.Println("服务器即将开启，访问地址 http://localhost:8080")
-
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        fmt.Println("服务器开启错误: ", err)
-    }
+func hello(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("hello liwenzhou.com!"))
 }
