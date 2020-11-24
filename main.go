d526186a2a6383939e9778e9eb8a389ecf2cb95c
birchgrove <birchgrove0612@gmail.com>
2020-11-24 22:25:23 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/goproxy/goproxy"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
-	server := &http.Server{
-		Addr: ":8888",
-	}
-  fmt.Println("server startup...")
-	if err := server.ListenAndServe(); err != nil {
-		fmt.Printf("server startup failed, err:%v\n", err)
-	}
-}
-
-func hello(w http.ResponseWriter, _ *http.Request) {
-	w.Write([]byte("hello liwenzhou.com!"))
+	http.ListenAndServe("0.0.0.0:8899", goproxy.New())
 }
