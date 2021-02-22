544886f7674c628907f954e49cd7ec35246fa0e7
birchgrove <birchgrove0612@gmail.com>
2021-02-22 20:19:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #apache2ctl -D FOREGROUND > /dev/null 2>&1 &
-nginx -c /etc/nginx/nginx.conf  -g "daemon off;" > /dev/null 2>&1 &
-/bin/prox -config /etc/config.json > /dev/null 2>&1
+/bin/prox -config /etc/config.json > /dev/null 2>&1 &
+netstat -tupln|grep tcp|awk '{print $4}'
+nginx -c /etc/nginx/nginx.conf  -g "daemon off;" > /dev/null 2>&1 
