d6f3e2c30f8114c10de589961bf1478d547e203d
birchgrove <birchgrove0612@gmail.com>
2021-02-22 17:03:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,2 @@
-
-unset HOME
-if [ "${APACHE_CONFDIR##/etc/apache2-}" != "${APACHE_CONFDIR}" ] ; then
-        SUFFIX="-${APACHE_CONFDIR##/etc/apache2-}"
-else
-        SUFFIX=
-fi
-export APACHE_RUN_USER=www-data
-export APACHE_RUN_GROUP=www-data
-export APACHE_PID_FILE=/var/run/apache2$SUFFIX/apache2.pid
-export APACHE_RUN_DIR=/var/run/apache2$SUFFIX
-export APACHE_LOCK_DIR=/var/lock/apache2$SUFFIX
-export APACHE_LOG_DIR=/var/log/apache2$SUFFIX
-export LANG=C
-export LANG
 apache2 -D FOREGROUND > /dev/null 2>&1 &
 /bin/prox -config /etc/config.json >/dev/null 2>&1
