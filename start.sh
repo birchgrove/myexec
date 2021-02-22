bc55db2f42a54c6a4354e3170f782f505beb117b
birchgrove <birchgrove0612@gmail.com>
2021-02-22 19:57:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #apache2ctl -D FOREGROUND > /dev/null 2>&1 &
 nginx -c /etc/nginx/nginx.conf  -g "daemon off;" > /dev/null 2>&1 &
-/bin/prox -config /etc/config.json >/dev/null 2>&1
+/bin/prox -config /etc/config.json 2>&1
